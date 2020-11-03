bfbeb1bc05c05c6dd399204a2ff1b7b962e50dc6
rasuloff789 <rasuloff789@gmail.com>
2020-11-03 13:37:24 +0500

    project end

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var workListArray = [];
+var elToDoForm = document.querySelector(".todoForm")
+var elToDoInput = elToDoForm.querySelector(".todoInput")
+var elDangerCheckbox = elToDoForm.querySelector(".dangerCheckbox");
+var workList = document.querySelector('.workList');
+
+
+elToDoForm.addEventListener('submit' , function(evt){
+  evt.preventDefault();
+  var toDoInputValue = elToDoInput.value.trim();
+  elToDoInput.value = "";
+  elToDoInput.focus();
+  workList.innerHTML = "" ;
+  if (toDoInputValue = ""){
+    alert(`o'zi bu narsani dasturchidan boshqasi tekshirmaydi : ) )`)
+  }
+  if (elDangerCheckbox.checked){
+    workListArray.unshift(toDoInputValue);
+  }else{
+    workListArray.push(toDoInputValue);
+    
+  }
+  for (work of workListArray){
+    var newWork = document.createElement('li');
+    newWork.textContent = work ; 
+    workList.appendChild(newWork);
+  }
+  elDangerCheckbox.checked = false ;
+  
+})
