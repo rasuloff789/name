08f7a3fad35f5a017f8043102614f8a42fb5d9d9
rasuloff789 <rasuloff789@gmail.com>
2020-11-20 17:33:23 +0500

    project end

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,33 @@ var elToDoForm = document.querySelector(".todoForm")
 var elToDoInput = elToDoForm.querySelector(".todoInput")
 var elDangerCheckbox = elToDoForm.querySelector(".dangerCheckbox");
 var workList = document.querySelector('.workList');
-
+var workTemplate = document.querySelector("#work-template").content;
 //formani tinglaymiz
+
+var createWorkElement = function(i){
+  var elNewWork = workTemplate.cloneNode(true) ;
+  var resultText = elNewWork.querySelector(".js-result-text");
+  resultText.textContent = i.work ;
+  return elNewWork;
+};
+
+var renderWorks = function (works) {
+  
+  workList.innerHTML = "" ;
+  
+  var elWorkWrapperFragment = document.createDocumentFragment();
+  
+  works.forEach(function (work) {
+    elWorkWrapperFragment.appendChild(createWorkElement(work));
+  });
+  
+  workList.appendChild(elWorkWrapperFragment);
+};
+
 elToDoForm.addEventListener('submit' , function(evt){
   //formani yuborilishini to'xtatamiz
   evt.preventDefault();
   //natijani htmllini boshida bo'shatib olamiz
-  workList.innerHTML = "" ;
   // inputdan valusini olamiz
   var toDoInputValue = elToDoInput.value.trim();
   //inputga hechnarsa yozilmaganda nima chiqishi
@@ -19,21 +39,73 @@ elToDoForm.addEventListener('submit' , function(evt){
     alert(`o'zi bu narsani dasturchidan boshqasi tekshirmaydi : ) )`)
   }
   
+  var addedObject = {
+    work : toDoInputValue,
+    isUrgent : true ,
+    completed : false 
+  };
+  
   //checked qilib yuborilgan element arrayning boshiga qo'shib qo'yiladi
   if (elDangerCheckbox.checked){
-    workListArray.unshift(toDoInputValue);
+    workListArray.push(addedObject);
   }else{
     //aks holda esa oxiriga
-    workListArray.push(toDoInputValue);
+    addedObject.isUrgent = false ;
+    workListArray.push(addedObject);
   }
-  for (work of workListArray){
-    // har bir elementni tanlab olib ularni ekranga chiqaramiz
-    var newWork = document.createElement('li');
-    newWork.textContent = work ; 
-    workList.appendChild(newWork);
-  }
+  
+  workListArray.sort(function(b , a){
+    if(a.isUrgent){
+      return 1;
+    };
+    if(b.isUrgent){
+      return -1;
+    };
+    return 0;
+  });
+  renderWorks(workListArray);
+  var allMessages = document.querySelectorAll(".js-result-text");
+  var endWork = function(){
+    workListArray.forEach(function(work , i){
+      if(work.completed){
+        allMessages[i].classList.add('text-dotted');
+      }else{
+        allMessages[i].classList.remove('text-dotted');
+      };
+    });
+  };
+  
+  var  completeWork = document.querySelectorAll(".js-checkbox");
+  completeWork.forEach(function(btn , i ){
+    btn.addEventListener('change' , function(){
+      if(btn.checked){
+        workListArray[i].completed = true ; 
+      }else{
+        workListArray[i].completed = false ; 
+      }
+      endWork();
+    });
+  });
+  
+  
+  var deleteWorkBtns = document.querySelectorAll(".js-delete-btn");
+  var elWork = document.querySelectorAll(".js-work");
+  
+  
+  deleteWorkBtns.forEach(function(btn , i ){
+    btn.addEventListener('click' , function(){
+      elWork[i].remove();
+      workListArray.splice(i , 1);
+    });
+  });
+  
+  
+  
   // oxirida inputni valuesini bo'shatib fozusni inputga qaratamiz va yana checkboxni checked holatini o'chirib qo'yamiz
   elToDoInput.value = "";
   elToDoInput.focus();
   elDangerCheckbox.checked = false ;
 })
+
+
+
